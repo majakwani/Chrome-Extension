894797f6f09b5aa7842a68b1bad2fae5a004463a
majakwani <anasjakwani72@gmail.com>
2022-11-30 20:45:50 +0500

    Wrote script to delete all leads and to save current tab as a lead

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-let myLeads = []
-const inputEl = document.querySelector("#input-el")
-const inputBtn = document.querySelector("#input-btn")
-const ulEl = document.querySelector("#ul-el")
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
+const saveTabBtn = document.querySelector("#tab-btn");
 
-inputBtn.addEventListener('click', function clickBtn(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-function renderLeads(){
-    let listItems = ""
-    for(let i =0; i < myLeads.length; i++){
-        listItems += `<li> <a target = "_blank" href = "${myLeads[i]}"> ${myLeads[i]} </a></li>`
-    }
-    ulEl.innerHTML = listItems
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads(myLeads);
+}
+
+saveTabBtn.addEventListener("click", function saveTab() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      renderLeads(myLeads);
+  })
+  
+});
+
+deleteBtn.addEventListener("dblclick", function clearLeads() {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads(myLeads);
+});
+
+inputBtn.addEventListener("click", function clickBtn() {
+  myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  inputEl.value = "";
+  renderLeads(myLeads);
+});
+
+function renderLeads(arr) {
+  let listItems = "";
+  for (let i = 0; i < arr.length; i++) {
+    listItems += `
+        <li>
+         <a target = "_blank" href = "${myLeads[i]}">
+          ${myLeads[i]}
+           </a>
+           </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
